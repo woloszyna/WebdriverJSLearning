3dbfe58cb4875fe1f2970c0e8d21f66bc17361c0
Albert Woloszyn <awoloszyn@gmail.com>
2020-08-15 08:19:27 +0100

    Switching windows

---
+++ JS_files/Switching Windows/adoption.js
@@ -0,0 +1,12 @@
+var webdriver = require('selenium-webdriver');
+var BasePage = require('./base');
+var By = webdriver.By;
+
+class AdoptionPage extends BasePage{
+
+    getLionAvailability() {
+        driver.findElement(By.id('check_btn_01')).click();
+    }
+}
+module.exports = new AdoptionPage();
+
